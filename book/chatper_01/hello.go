71e7b337efffefca4e6c3224beb664a3306c5b36
shiva <sreddy@hey.com>
2025-03-16 20:35:52 -0400

    .

--- book/chatper_01/hello.go
+++ book/chatper_01/hello.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
+
+	fmt.Println("Hello %s there", "world")
+	var x int
+	x = 21
+	fmt.Println("x=", x)
+	x += 1
+	fmt.Println("x=", x)
+
+	var f bool
+	f = false
+	fmt.Println(f)
+	f = true
+	fmt.Println(f)
+
+	a := 10
+	fmt.Println("a=", a)
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
 	fmt.Println("Hello World")
 }
