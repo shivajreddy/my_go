7755c7d32d761b845a3ca051197532893eb360d5
shiva <sreddy@hey.com>
2025-03-18 21:49:44 -0400

    .

--- book/chatper_01/hello.go
+++ book/chatper_01/hello.go
@@ -1,25 +1,21 @@
+/*
+Create a Linked List in GO
+*/
 package main
 
 import "fmt"
 
+type Node struct {
+	val  int
+	next *Node
+}
+
+// ENTRY POINT
 func main() {
+	n := Node{}
 
-	fmt.Println("Hello %s there", "world")
-	var x int
-	x = 21
-	fmt.Println("x=", x)
-	x += 1
-	fmt.Println("x=", x)
-
-	var f bool
-	f = false
-	fmt.Println(f)
-	f = true
-	fmt.Println(f)
-
-	a := 10
-	fmt.Println("a=", a)
-	fmt.Println("go" + "lang")
-	fmt.Println("1+1 =", 1+1)
-	fmt.Println("Hello World")
+	fmt.Println(n.val)
+	fmt.Println(n)
+	fmt.Println(nil)
+	fmt.Println(n.next)
 }
