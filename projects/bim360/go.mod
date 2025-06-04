998522faadc29b81f1fa71c471b08b1cb526914c
shiva <sreddy@hey.com>
2025-06-04 13:50:50 -0400

    .

--- projects/bim360/go.mod
+++ projects/bim360/go.mod
@@ -1,3 +1,5 @@
 module bim360
 
 go 1.24
+
+require github.com/joho/godotenv v1.5.1
--- projects/bim360/main.go
+++ projects/bim360/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("BIM 360 API TEST")
+
+	// Load the env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("couldn't load the .env file")
+	}
+
+	// Access the environment variables
+	client_id := os.Getenv("CLIENT_ID")
+	client_secret := os.Getenv("CLIENT_SECRET")
+
+	fmt.Println("client_id:", client_id)
+	fmt.Println("client_secret:", client_secret)
 }
